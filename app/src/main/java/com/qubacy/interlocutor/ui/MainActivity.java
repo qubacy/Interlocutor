cf96d43ecc7734b756c8a654561b2ba1708cba9b
MCDead <souldzukaev@gmail.com>
2023-06-25 23:34:20 +0700

    Main Menu screen has been created;

--- app/src/main/java/com/qubacy/interlocutor/ui/MainActivity.java
+++ app/src/main/java/com/qubacy/interlocutor/ui/MainActivity.java
@@ -1,14 +1,17 @@
-package com.qubacy.interlocutor;
+package com.qubacy.interlocutor.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.qubacy.interlocutor.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/com/qubacy/interlocutor/ui/screen/main/MainMenuFragment.java
@@ -0,0 +1,44 @@
+package com.qubacy.interlocutor.ui.screen.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.qubacy.interlocutor.R;
+
+public class MainMenuFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_main_menu, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        AppCompatActivity appCompatActivity = ((AppCompatActivity)getActivity());
+
+        if (appCompatActivity == null) return;
+        if (appCompatActivity.getSupportActionBar() == null) return;
+
+        appCompatActivity.getSupportActionBar().hide();
+    }
+}
