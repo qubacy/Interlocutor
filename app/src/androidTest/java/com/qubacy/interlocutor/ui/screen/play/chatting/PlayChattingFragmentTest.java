1489a07ca7cf5768e710663eaf194079245ee1e6
MCDead <souldzukaev@gmail.com>
2023-07-02 18:33:05 +0700

    Working on PlayChattingFragment's appearance;

---
+++ app/src/androidTest/java/com/qubacy/interlocutor/ui/screen/play/chatting/PlayChattingFragmentTest.java
@@ -0,0 +1,33 @@
+package com.qubacy.interlocutor.ui.screen.play.chatting;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.fragment.app.testing.FragmentScenario;
+import androidx.lifecycle.Lifecycle;
+import androidx.test.espresso.Espresso;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class PlayChattingFragmentTest {
+    private Context m_context = null;
+    private FragmentScenario m_fragmentScenario = null;
+
+    @Before
+    public void setUp() {
+        m_context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        m_fragmentScenario = FragmentScenario.launchInContainer(PlayChattingFragment.class);
+    }
+
+    @Test
+    public void testPlayChattingFragmentAppearance() {
+        m_fragmentScenario.moveToState(Lifecycle.State.STARTED);
+
+    }
+
+}
